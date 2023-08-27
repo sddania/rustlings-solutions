efe4e19c155d4dd70319a479d7480d94db03e8ac
Daniele Agosti <sddania@gmail.com>
2023-08-27 12:21:13 +0200

    finish all exercice until quiz1 and quiz

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,10 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(numOfApple: i32) -> i32 {
+    let price = if (numOfApple > 40) { 1 } else { 2 };
+    numOfApple * price
+}
 
 // Don't modify this function!
 #[test]
